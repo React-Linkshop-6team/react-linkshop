8c5b6eb75c854f8e0e58b30e321fe5253b7f3754
Hyun <ikimyh059554@gmail.com>
2025-04-12 09:38:07 +0900

    feat: 페이지 분기 처리

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import ProfileDetail from './pages/ProfileDetail'
+import Create from './pages/Create'
+import Edit from './pages/Edit'
+
 const App = () => {
-  return <div>hi~ this is tesecode test hyun</div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<ProfileDetail />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/edit" element={<Edit />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
