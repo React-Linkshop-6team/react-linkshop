a7450abd4f4968668a16b6807880dda9c2faeeb0
Hyun <ikimyh059554@gmail.com>
2025-04-14 14:26:59 +0900

    fix: 병합 충돌 해결

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-<<<<<<< HEAD
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
 import ProfileDetail from './pages/ProfileDetail'
 import Create from './pages/Create'
 import Edit from './pages/Edit'
-=======
+
 import './assets/scss/Style.scss'
->>>>>>> dev
 
 const App = () => {
   return (
