dc1fa0704878cecf44011a431ef1f7095d7e0b8f
Hyun <ikimyh059554@gmail.com>
2025-04-14 16:45:57 +0900

    feat: 버튼 children 받아서 보여지도록 공통 컴포넌트화

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,5 +1,19 @@
-const Button = () => {
-  return <div>button</div>
+import { Link } from 'react-router-dom'
+
+const Button = ({ children, to, className = '', type = 'button', onClick }) => {
+  if (to) {
+    return (
+      <Link to={to} className={`button ${className}`}>
+        {children}
+      </Link>
+    )
+  }
+
+  return (
+    <button type={type} onClick={onClick} className={`button ${className}`}>
+      {children}
+    </button>
+  )
 }
 
 export default Button
