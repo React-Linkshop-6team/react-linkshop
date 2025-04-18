55f7ede52d58faaf94935eedc9a68c89f3a11944
Hyun <ikimyh059554@gmail.com>
2025-04-18 19:49:51 +0900

    feat: 좋아요 눌렀을 때 post/delete 호출

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const LINKSHOP_API_URL = 'https://linkshop-api.vercel.app/15-6/linkshops'
+const LINKSHOP_API_URL = import.meta.env.VITE_LINKSHOP_API_URL
 
 export const getShops = async () => {
   try {
@@ -9,5 +9,27 @@ export const getShops = async () => {
   } catch (error) {
     console.error('에러가 발생했습니다.', error)
     return []
+  }
+}
+
+// 좋아요 버튼 눌렀을 때 등록
+export const addLike = async shopId => {
+  try {
+    const response = await axios.post(`${LINKSHOP_API_URL}/${shopId}/like`)
+    return response.status === 200 || response.status === 201
+  } catch (error) {
+    console.error('좋아요 실패', error)
+    return false
+  }
+}
+
+// 좋아요 취소 버튼 눌렀을 때 등록
+export const removeLike = async shopId => {
+  try {
+    const response = await axios.delete(`${LINKSHOP_API_URL}/${shopId}/like`)
+    return response.status === 200
+  } catch (error) {
+    console.error('좋아요 취소 실패', error)
+    return false
   }
 }
