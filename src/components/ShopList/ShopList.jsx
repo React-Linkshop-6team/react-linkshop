87b13275fafb76237ac2492e119b54985822ea06
Hyun <ikimyh059554@gmail.com>
2025-04-18 19:52:11 +0900

    refactor: 상점 목록 필터링 및 정렬 로직 간소화

--- src/components/ShopList/ShopList.jsx
+++ src/components/ShopList/ShopList.jsx
@@ -8,22 +8,14 @@ import polygonFilter from '../../assets/images/polygon-filter.png'
 
 // ShopList 컴포넌트
 // 역할: 상점 목록을 렌더링하는 컴포넌트
-const ShopList = ({ list }) => {
+const ShopList = ({ list = [] }) => {
   const [searchItem, setSearchItem] = useState('') // 사용자가 입력한 검색어
   const [isFilterOpen, setIsFilterOpen] = useState(false) // 필터 모달 창이 열려있는지 여부를 저장하는 상태 변수
   const [selectedFilter, setSelectedFilter] = useState('상세필터') // 선택된 필터값 (Ex. 최신순, 좋아요순)
 
-  const updatedList = list.map(shop => {
-    const savedCount = localStorage.getItem(`likeCount_${shop.id}`)
-    return {
-      ...shop,
-      likes: savedCount !== null ? parseInt(savedCount, 10) : shop.likes,
-    }
-  })
-
   // 1. 상점 이름에 검색어가 포함된 것 만 골라냄.
   // 2. 사용자가 선택한 필터 값에 맞춰 정렬
-  const filteredList = updatedList
+  const filteredList = list
     .filter(shop => shop.name.toLowerCase().includes(searchItem.toLowerCase()))
     .sort((a, b) => {
       if (selectedFilter === '최신순') return new Date(b.createdAt) - new Date(a.createdAt)
