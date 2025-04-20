d2ab03f1c7242373d53d985f609aa0fdbea1adae
seong5 <greenbi0852@gmail.com>
2025-04-21 02:14:48 +0900

    feat-48-gotoeditpage

--- src/components/InputPasswordModal.jsx
+++ src/components/InputPasswordModal.jsx
@@ -1,9 +1,31 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 const InputPasswordModal = () => {
+  const [password, setPassword] = useState('')
+  const goEdit = useNavigate()
+
+  const handleClickPassword = () => {
+    if (password === 'asdd') {
+      goEdit('/edit')
+    } else {
+      alert('비밀번호를 확인해주세요.')
+    }
+  }
+
   return (
     <section className="password-modal">
       <p className="password-messege">비밀번호를 입력해주세요 🙏</p>
-      <input className="input-password" placeholder="비밀번호를 입력해주세요." />
-      <button className="check-button">확인</button>
+      <input
+        className="input-password"
+        placeholder="비밀번호를 입력해주세요."
+        type="password"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      />
+      <button className="check-button" onClick={handleClickPassword}>
+        확인
+      </button>
     </section>
   )
 }
