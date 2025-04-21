3c5c90204ad9827687a21de67cdc2ffb5465a86e
이유나 <iyuna@iyunaui-noteubug.local>
2025-04-21 16:19:05 +0900

    feat/ api.js에 create api 함수 등록

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
-
 const LINKSHOP_API_URL = import.meta.env.VITE_LINKSHOP_API_URL
 
 export const getShops = async () => {
@@ -12,7 +11,6 @@ export const getShops = async () => {
     return []
   }
 }
-
 
 export default getShops
 
@@ -28,7 +26,7 @@ export const getShopById = async id => {
     return null
   }
 }
-    
+
 // 좋아요 버튼 눌렀을 때 등록
 export const addLike = async shopId => {
   try {
@@ -48,6 +46,34 @@ export const removeLike = async shopId => {
   } catch (error) {
     console.error('좋아요 취소 실패', error)
     return false
+  }
+}
 
+// 생성하기 API 요청
+export const createShop = async payload => {
+  try {
+    const response = await axios.post(LINKSHOP_API_URL, payload, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    return response.data
+  } catch (error) {
+    console.error('❌ 등록 실패:', error)
+    throw error.response?.data || error
+  }
+}
+
+// 이미지 업로드
+export const uploadImage = async file => {
+  const formData = new FormData()
+  formData.append('image', file)
+
+  try {
+    const response = await axios.post(IMAGE_UPLOAD_URL, formData)
+    return response.data.url
+  } catch (error) {
+    console.error('이미지 업로드 실패', error)
+    return null
   }
 }
