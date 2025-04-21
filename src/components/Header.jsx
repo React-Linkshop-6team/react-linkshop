8952797b23776842264b489bc3cc9a61cb12d21d
이유나 <iyuna@iyunaui-noteubug.local>
2025-04-21 16:23:04 +0900

    feat/ create.jsx에서 헤더 부분에 돌아가기로 버튼 변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,29 @@
+// import { useLocation, Link } from 'react-router-dom'
+
+// import logo from '../assets/images/logo.png'
+// import Button from './common/Button'
+
+// const Header = () => {
+//   const location = useLocation()
+
+//   if (location.pathname === '/profile') return null
+
+//   return (
+//     <header className="header">
+//       <div className="header-left">
+//         <Link to="/">
+//           <img src={logo} alt="Linkshop logo" className="header-logo" />
+//         </Link>
+//       </div>
+//       <div className="header-right">
+//         <Button to="/create">생성하기</Button>
+//       </div>
+//     </header>
+//   )
+// }
+
+// export default Header
+
 import { useLocation, Link } from 'react-router-dom'
 
 import logo from '../assets/images/logo.png'
@@ -6,6 +32,7 @@ import Button from './common/Button'
 const Header = () => {
   const location = useLocation()
 
+  // 현재 위치가 /profile 페이지라면 헤더를 렌더링하지 않음
   if (location.pathname === '/profile') return null
 
   return (
@@ -16,7 +43,11 @@ const Header = () => {
         </Link>
       </div>
       <div className="header-right">
-        <Button to="/create">생성하기</Button>
+        {location.pathname === '/create' ? (
+          <Button to="/list">돌아가기</Button>
+        ) : (
+          <Button to="/create">생성하기</Button>
+        )}
       </div>
     </header>
   )
