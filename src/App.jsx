ffce401271c69e4c196509ec58b75d5def86c986
Hyun <ikimyh059554@gmail.com>
2025-04-23 23:20:47 +0900

    feat: 회원가입 라우팅 설정

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import ProfileDetail from './pages/ProfileDetail'
 import Create from './pages/Create'
-//import Edit from './pages/Edit'
 import Header from './components/Header'
 import EditMyshop from './components/common/Edit/EditMyshop'
 import CreateMyshop from './components/common/Create/CreateMyshop'
-
 import './assets/scss/Styles.scss'
+import Signup from './pages/Signup'
 
 const App = () => {
   return (
@@ -19,7 +18,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/profile/:id" element={<ProfileDetail />} />
           <Route path="/create" element={<Create />} />
-          {/* <Route path="/edit" element={<Edit />} /> */}
+          <Route path="/signup" element={<Signup />} />
           <Route path="/myshop" element={<CreateMyshop />} />
           <Route path="/myshop" element={<EditMyshop />} />
         </Routes>
