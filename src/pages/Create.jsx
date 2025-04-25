37a843f1cb967821095389ff316d80d0e1169796
Hyun <ikimyh059554@gmail.com>
2025-04-25 20:26:45 +0900

    fix: package-lock conflict가 계속 나고 있음 일단 해결

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { useLocation } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import CreateRepItem from '../components/common/Create/CreateRepItem'
 import CreateMyshop from '../components/common/Create/CreateMyshop'
 import { createShop } from '../api/api'
